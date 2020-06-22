f5ad5e45f3a097d323c47c2e2c5e27f78f4480e2
bashi-bashot <axel.portigliatti26@live.fr>
2020-06-22 10:48:03 +0200

    optimisation fromCSVtoSQL 01

--- LILAS/communication/fromCSVtoSQL.py
+++ LILAS/communication/fromCSVtoSQL.py
@@ -130,11 +130,20 @@ def createAppel(t, listeLif):
             
             date_a_sauvegarder.save()
 
+        
+        #On vérifie si l'appel est un doublon :
+        # On part de la DATE de l'appel, puis on prend tous les appels à cette date-là. 
+
+        listeDAppels = Date.objects.filter(date__exact = d.date())[0].Appel.all() #Liste de tous les appels qui ont été passés à la date d déjà sauvegardés.
+
         #if Appel.objects.filter(date__date__contains=d.date(), heure__contains=d.time(), appelant__contains=apple, line_appelante__contains=fsx_e, appele__contains=applant, line_appele__contains=fsx_a).count()==1:
-        if Appel.objects.filter(date__date__contains=d.date()).filter(heure__contains=d.time()).filter(appelant__contains=apple).filter(line_appelante__contains=fsx_e).filter(appele__contains=applant).filter(line_appele__contains=fsx_a).count()==1:
+        #if Appel.objects.filter(date__date__contains=d.date()).filter(heure__contains=d.time()).filter(appelant__contains=apple).filter(line_appelante__contains=fsx_e).filter(appele__contains=applant).filter(line_appele__contains=fsx_a).count()==1:
+        #    print("Doublon :"+d.__str__()+" "+applant+" "+apple)
+        #    pass
+        if listeDAppels.objects.filter(date__date__contains=d.date()).filter(heure__contains=d.time()).filter(appelant__contains=apple).filter(line_appelante__contains=fsx_e).filter(appele__contains=applant).filter(line_appele__contains=fsx_a).count()==1:
             print("Doublon :"+d.__str__()+" "+applant+" "+apple)
             pass
-    
+            
         else:
             dur = dfin - d #dur n'est pas un objet datetime, mais un objet timedelta
             

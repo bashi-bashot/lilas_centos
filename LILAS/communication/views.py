7d3a0e57f7cc530c927b99344ae622dc4775067a
bashi-bashot <axel.portigliatti26@live.fr>
2020-06-29 16:54:29 +0200

    com : correction bug secteur et corr

--- LILAS/communication/views.py
+++ LILAS/communication/views.py
@@ -138,7 +138,7 @@ def index(request):
                      #RECUPERATION DU CHAMP DU MENU DEROULANT DE SELECTION DES SECTEURS
 
             strSecteur = formulaireDates.cleaned_data['positionSpinner']   
-            choixSpinner = formulaireDates.fields['positionSpinner'].choices[int(strSecteur)]
+            choixSpinner = formulaireDates.fields['positionSpinner'].choices[int(strSecteur)-1]
             
             #Il faut maintenant affiner la liste listeDates pour n'afficher que les appels faisant intervenit le secteur choisi
             #print(listeDates.count())
@@ -149,7 +149,7 @@ def index(request):
                      #RECUPERATION DU CHAMP DU MENU DEROULANT DE SELECTION DES CORRESONDANTS ET AFFINAGE DE LA LISTE D'APPELS
                     
             strCorr = formulaireDates.cleaned_data['correspondantSpinner']
-            choixSpinner_corr = formulaireDates.fields['correspondantSpinner'].choices[int(strCorr)]
+            choixSpinner_corr = formulaireDates.fields['correspondantSpinner'].choices[int(strCorr)-1]
                    
             #------------------------------------------------------------------------------------------
                      #POPULATION DU MENU DEROULANT DANS LEQUEL ON CHOISIT LE SECTEUR

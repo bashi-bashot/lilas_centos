fd5ea5dc5d1ef5254310add8ebbbf14d4e0298fe
bashi-bashot <axel.portigliatti26@live.fr>
2020-07-08 11:03:41 +0200

    inc : pre remplissage champs dates

--- LILAS/incident/forms.py
+++ LILAS/incident/forms.py
@@ -5,5 +5,4 @@ class DateForm(forms.Form):
     dateDebut = forms.DateField(label='Date de début : ', widget=widgets.AdminDateWidget())
     heureDebut  = forms.CharField(max_length=8, widget=forms.TextInput(attrs={'size':10, 'value':'00:00:00'}))
     dateFin = forms.DateField(label='Date de fin : ',  widget=widgets.AdminDateWidget())
-    heureFin = forms.CharField(label='Heure de Fin (xx:xx:xx)', max_length=8)
-
+    heureFin = forms.CharField(max_length=8, widget=forms.TextInput(attrs={'size':10, 'value':'23:59:59'}))

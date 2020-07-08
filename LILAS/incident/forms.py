7f857187a32158659b7e2dd285d92d7e16098ef9
bashi-bashot <axel.portigliatti26@live.fr>
2020-07-08 11:02:16 +0200

    inc : pre remplissage champs dates

--- LILAS/incident/forms.py
+++ LILAS/incident/forms.py
@@ -3,7 +3,7 @@ from django.contrib.admin import widgets
 
 class DateForm(forms.Form):
     dateDebut = forms.DateField(label='Date de début : ', widget=widgets.AdminDateWidget())
-    heureDebut  = forms.CharField(label='Heure de début (xx:xx:xx)', max_length=8)
+    heureDebut  = forms.CharField(max_length=8, widget=forms.TextInput(attrs={'size':10, 'value':'00:00:00'}))
     dateFin = forms.DateField(label='Date de fin : ',  widget=widgets.AdminDateWidget())
     heureFin = forms.CharField(label='Heure de Fin (xx:xx:xx)', max_length=8)
 

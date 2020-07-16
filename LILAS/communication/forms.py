a2c3f168cad26b181682d7e7e74bacb49b5f47a0
bashi-bashot <axel.portigliatti26@live.fr>
2020-07-16 14:06:54 +0200

    modif chargement conf artemis

--- LILAS/communication/forms.py
+++ LILAS/communication/forms.py
@@ -27,7 +27,9 @@ choicesSecteurs2 = [(i+2, (bddSecteur[i].nom)) for i in range(bddSecteur.count()
 choiceExterieur2= [(i+2, (bddExterieur[i].nom)) for i in range(bddExterieur.count())]
 
 choicesSecteurs = choicesSecteurs + choicesSecteurs2
-choiceExterieur = choiceExterieur + choiceExterieur2                                                                                                                                                                                                                       
+choiceExterieur = choiceExterieur + choiceExterieur2      
+
+choiceExterieur.sort(key=lambda x:x[1])
 
        
 class NameForm(forms.Form):

0513086ea0203ffaf2830810cc45094e4eb0ab50
bashi-bashot <axel.portigliatti26@live.fr>
2020-10-23 11:17:21 +0200

    suppression des guillemets des faisceaux et des lifs a lajout dune nouvelle conf artemis

--- LILAS/communication/loadFX.py
+++ LILAS/communication/loadFX.py
@@ -120,8 +120,8 @@ for i in range(len(tabIndexLif)):
         if k[0] == '24' :
             print("Association LIF-FAISCEAU trouvee")
             print(str(id_floating))
-            print(k[2][0:len(k[2])-4])
-            if k[2][0:len(k[2])-4] == str(id_floating) : #Si on trouve l'id_elts d'une LIF dans le tableau d'association, alors il faut regarder l'id_elts du faisceau
+            print(k[2][0:len(k[2])-3])
+            if k[2][0:len(k[2])-3] == str(id_floating) : #Si on trouve l'id_elts d'une LIF dans le tableau d'association, alors il faut regarder l'id_elts du faisceau
                 id_fx = k[1][0:len(k[1])-3]
                 print("Faisceau trouve avec l'id : "+id_fx)
             #else :

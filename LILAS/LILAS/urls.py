cbb1efc9e98e64b2687ab95d3241806263e536d8
bashi-bashot <axel.portigliatti26@live.fr>
2020-12-15 09:42:24 +0100

    ajout favicon

--- LILAS/LILAS/urls.py
+++ LILAS/LILAS/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+#Ajouts pour le favicon :
+from django.views.generic.base import RedirectView
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.conf import settings
 
 urlpatterns = [
+    path("favicon.ico", RedirectView.as_view(url=staticfiles_storage.url("favicon.ico")),
     path('communication/', include('communication.urls')),
     path('configSalle/', include('configSalle.urls')),
     path('incident/', include('incident.urls')),
